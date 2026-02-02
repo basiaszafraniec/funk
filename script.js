adf3ada4e16fe4ccc7fc1e6f18953bbf87599ebc
basiaszafraniec <basia.szafraniec@gmail.com>
2026-02-02 16:00:42 +0100

    files open somewhat

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ let offsetX = 0;
 let offsetY = 0;
 let topZ = 10;
 
+const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
 
 //create window on clicking file
-function createWindow() {
+function createWindow(content = "Empty File") {
     const win = document.createElement("div");
     win.className = "window";
     win.style.width = "200px";
@@ -33,15 +35,17 @@ function createWindow() {
         </div>
       </div>
     </div>
-    <div class="bottom-container"></div>
+    <div class="bottom-container">${content}</div>
   `;
 
     document.body.appendChild(win);
 }
 
 document.querySelectorAll(".file").forEach(file => {
-    file.addEventListener("click", () => {
-        createWindow();
+    file.addEventListener("click", (e) => {
+        content = e.target.id;
+        if (file.dataset.open) return;
+        createWindow(content);
     });
 });
 
@@ -54,7 +58,11 @@ document.addEventListener("click", (e) => {
 
 
 //move windows around 
-document.addEventListener("mousedown", (e) => {
+document.addEventListener("pointerdown", (e) => {
+    if (e.target.closest("input, button, select, textarea")) return;
+    if (e.pointerType === "touch") {
+        e.preventDefault();
+    };
     if (e.target.closest(".x-div")) return;
     const topBar = e.target.closest(".top-bar");
     if (!topBar) return;
@@ -73,21 +81,21 @@ document.addEventListener("mousedown", (e) => {
     topBar.style.cursor = "grabbing";
 });
 
-document.addEventListener("mousemove", (e) => {
+document.addEventListener("pointermove", (e) => {
     if (!activeWindow) return;
 
     activeWindow.style.left = e.clientX - offsetX + "px";
     activeWindow.style.top = e.clientY - offsetY + "px";
 });
 
-document.addEventListener("mouseup", () => {
+document.addEventListener("pointerup", () => {
     if (!activeWindow) return;
     activeWindow.querySelector(".top-bar").style.cursor = "grab";
     activeWindow = null;
 });
 
 //bring to front on click
-document.addEventListener("mousedown", (e) => {
+document.addEventListener("pointerdown", (e) => {
     const win = e.target.closest(".window");
     if (win && win.id === "main-window") return;
     if (!win) return;
@@ -102,5 +110,32 @@ const word = document.getElementById("word");
 
 colorPicker.addEventListener("input", (e) => {
     word.style.color = e.target.value;
+});
+
+//file tabs
+const pages = document.querySelectorAll(".files-page");
+const historyStack = ["root"];
+
+function showPage(page) {
+    pages.forEach(p => {p.classList.remove("active");});
+    document.querySelector(`.files-page[data-page="${page}"]`).classList.add("active");
+}
+
+document.querySelectorAll(".file-div").forEach(fileDiv => {
+    fileDiv.addEventListener("click", (e) => {
+        const opener = e.target.closest("[data-open]");
+        if (!opener) return;
+        const pageToOpen = opener.dataset.open;
+        historyStack.push(pageToOpen);
+        showPage(pageToOpen);
+    });
+});
+//aroow back 
+document.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("arrow-back")) return;
+
+  historyStack.pop(); // remove current
+  const previous = historyStack[historyStack.length - 1];
+  showPage(previous);
 });
 
